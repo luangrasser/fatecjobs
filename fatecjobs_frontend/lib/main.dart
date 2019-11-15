ad3f0026449b82de7ae06c388392310171a8a23e
luangrasser <bonilhaluan@gmail.com>
2019-11-15 14:45:37 -0300

    Commit inicial

---
+++ fatecjobs_frontend/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fatecjobs_frontend/widgets/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+  return MaterialApp(
+    title:'Fatec Jobs',
+    theme:ThemeData(primarySwatch: Colors.red),
+    home:Login(),
+  );
+  }
+}
---
+++ fatecjobs_frontend/lib/widgets/login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+               TextField(
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(color: Colors.blue, fontSize: 30),
+                decoration: InputDecoration(
+                  labelText:"E-mail do Usuário",
+                  labelStyle: TextStyle(color: Colors.black),
+                )
+           ),
+              Divider(),
+              TextField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.number,
+                style: TextStyle(color: Colors.black, fontSize: 30),
+                decoration: InputDecoration(
+                  labelText:"Senha",
+                  labelStyle: TextStyle(color: Colors.black),
+                )
+              ),
+              Divider(),
+              ButtonTheme(
+                height: 60.0,
+                child: RaisedButton(
+                  onPressed: () => {
+                    print("pressionei o botão"),
+                  },
+                  shape:  new RoundedRectangleBorder(borderRadius: 
+                          new BorderRadius.circular(30.0)),
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                  color:Colors.black,
+                ),
+              ),
+            ],
+         ),
+        ),
+      )
+   );
+  }
+}
